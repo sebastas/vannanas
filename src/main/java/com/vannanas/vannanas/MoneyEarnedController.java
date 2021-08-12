8cf00647699ea7b36619ec6b7f3706970f9b0c08
BOUVET\ane.kristiansen <ane.kristiansen@bouvet.no>
2021-08-12 10:50:44 +0200

    laget endepunkt for turbinstatus uten logikk

--- src/main/java/com/vannanas/vannanas/MoneyEarnedController.java
+++ src/main/java/com/vannanas/vannanas/MoneyEarnedController.java
@@ -3,8 +3,6 @@ package com.vannanas.vannanas;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-
 @RestController
 public class MoneyEarnedController {
 
---
+++ src/main/java/com/vannanas/vannanas/TurbineStatus.java
@@ -0,0 +1,13 @@
+package com.vannanas.vannanas;
+
+public class TurbineStatus {
+    public static boolean turbinStatus;
+
+    public static boolean isTurbinStatus() {
+        return turbinStatus;
+    }
+
+    public static void setTurbinStatus(boolean turbinStatus) {
+        TurbineStatus.turbinStatus = turbinStatus;
+    }
+}
